5a91916fea917d6cf16e1e18a5078d5a9ae8262b
Alex Ruiz <rgalex2034@hotmail.com>
2025-12-28 02:35:44 +0100

    Removed ThreadPoolError and fixed error messages.
    As with the previous commit, I was going to reimplement the
    ThreadPoolError to a proper enum. The error was use once, so I've
    decided to remove the custom error and just propagate the Sender error
    directly.
    
    Finally, I've got some error messages wrong. The .lock() error and
    .recv() error were swapped, and also not handled properly.
    
    Now they are handled properly, with the PosionedMutex error from
    .lock() beign ignored now, as it means a previous request panicked
    and in theory we could continue processing requests.

--- src/thread_pool.rs
+++ src/thread_pool.rs
@@ -1,9 +1,7 @@
 use std::{
-    error::Error,
-    fmt::Display,
     sync::{
         Arc, Mutex,
-        mpsc::{self, Receiver},
+        mpsc::{self, Receiver, SendError},
     },
     thread::{self, JoinHandle},
 };
@@ -32,16 +30,12 @@ impl ThreadPool {
         ThreadPool { workers, sender }
     }
 
-    pub fn execute<F>(&self, f: F) -> Result<(), ThreadPoolError>
+    pub fn execute<F>(&self, f: F) -> Result<(), SendError<Box<dyn FnOnce() + Send + 'static>>>
     where
         F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
-        self.sender.send(job).map_err(|error| {
-            ThreadPoolError(format!(
-                "ThreadPool: Could not send job to workers.\nInner: {error}"
-            ))
-        })
+        self.sender.send(job)
     }
 }
 
@@ -57,18 +51,6 @@ impl Drop for ThreadPool {
     }
 }
 
-#[derive(Debug)]
-pub struct ThreadPoolError(String);
-
-impl Display for ThreadPoolError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let Self(error) = self;
-        f.write_str(&format!("ThreadPool error: {error}"))
-    }
-}
-
-impl Error for ThreadPoolError {}
-
 struct Worker {
     id: usize,
     thread: JoinHandle<()>,
@@ -81,16 +63,18 @@ impl Worker {
                 println!("Worker[{id}]: Locking on receiver...");
                 let receiver = match receiver.lock() {
                     Ok(inner) => inner,
-                    Err(error) => {
-                        println!("Worker[{id}]: Thread shutting down: {error}");
-                        break;
+                    Err(poisoned_error) => {
+                        eprintln!(
+                            "Worker[{id}]: Poisoned mutex! Some previous request panicked. Ignoring error."
+                        );
+                        poisoned_error.into_inner()
                     }
                 };
 
                 println!("Worker[{id}]: Awaiting message...");
-                let job = receiver
-                    .recv()
-                    .expect(&format!("Worker[{id}] Poisoned mutex! Panicking."));
+                let job = receiver.recv().expect(&format!(
+                    "Worker[{id}] Thread shutting down. Sender closed the channel."
+                ));
 
                 // Drop the receiver before calling the job.
                 // This way, we release the lock, and other workers can start
